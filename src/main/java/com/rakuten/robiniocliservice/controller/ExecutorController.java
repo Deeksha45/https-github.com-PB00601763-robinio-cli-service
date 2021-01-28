45f5c12cebfe899e7c3823228f56e3705d1d74fb
Bhavesh Notwani <bn00602316@techmahindra.com>
2021-01-28 18:35:42 +0530

    changes dependency

--- src/main/java/com/rakuten/robiniocliservice/controller/ExecutorController.java
+++ src/main/java/com/rakuten/robiniocliservice/controller/ExecutorController.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,41 +15,37 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.minidev.json.JSONObject;
-
 @CrossOrigin("*")
 @RestController
 public class ExecutorController {
 
 	@PostMapping(value = "/executebash")
-	public String getExecutorResp(@RequestHeader HttpHeaders headers,@RequestBody JSONObject jsonObject) {
-		
-		String bashName = "scripts/"+jsonObject.getAsString("bashname");
-		String command = "sh "+bashName+" "+jsonObject.getAsString("bashparams")==null?"":jsonObject.getAsString("bashparams");
-		ProcessBuilder builder = new ProcessBuilder(command.trim());
-		builder.redirectErrorStream(true);
-		Process p = null;
+	public String getExecutorResp(@RequestHeader HttpHeaders headers, @RequestBody JSONObject jsonObject) {
 		try {
+			String bashName = "scripts/" + jsonObject.getString("bashname");
+			String command = "sh " + bashName + " " + jsonObject.getString("bashparams") == null ? ""
+					: jsonObject.getString("bashparams");
+			ProcessBuilder builder = new ProcessBuilder(command.trim());
+			builder.redirectErrorStream(true);
+			Process p = null;
+
 			p = builder.start();
-		} catch (IOException e) {
+
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = null;
+			while (true) {
+				line = r.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+
+			return line;
+		} catch (IOException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "{\"status\":\"error\"}";
 		}
-		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line = null;
-		while (true) {
-			try {
-				line = r.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			if (line == null) {
-				break;
-			}
-			System.out.println(line);
-		}
-
-		return line;
 	}
 }
