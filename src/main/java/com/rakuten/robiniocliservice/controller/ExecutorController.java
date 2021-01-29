49338f93b9d4d6926aa305f742a03ef9060fc7d6
RK00607336 <77377766+RK00607336@users.noreply.github.com>
2021-01-29 16:46:34 +0530

    Update ExecutorController.java
    logger added

--- src/main/java/com/rakuten/robiniocliservice/controller/ExecutorController.java
+++ src/main/java/com/rakuten/robiniocliservice/controller/ExecutorController.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @CrossOrigin("*")
 @RestController
 public class ExecutorController {
+	
+	private static Logger LOG = LoggerFactory.getLogger(ExecutorController.class);
 
 	@PostMapping(value = "/executebash", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public String getExecutorResp(@RequestHeader HttpHeaders headers, @RequestBody String requestStr) {
 		try {
 			JSONObject jsonObject = new JSONObject(requestStr);
-			System.out.println("jsonObject:::" + jsonObject);
+			LOG.info("jsonObject:::" + jsonObject);
 			String commandName = jsonObject.getString("commandname");
 			String bashName = jsonObject.getString("bashname");
 			String bashParams = jsonObject.getString("bashparams");
@@ -41,12 +46,19 @@ public class ExecutorController {
 				}
 				lineBuilder.append(line);
 			}
-			
+			if(!checkIsNullOrEmpty(lineBuilder.toString())) {
+			LOG.info("response from "+lineBuilder.toString());
+			}else {
+				LOG.info("response from getExecutorResp is null");
+			}
 			return lineBuilder.toString();
 		} catch (IOException | JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.error("Exception occured in getExecutorResp",e.getMessage());
 			return "{\"status\":\"error\"}";
 		}
+	}
+	
+	public static boolean checkIsNullOrEmpty(String value) {
+		return (value == null || value.equals("") || value.equalsIgnoreCase("null"));
 	}
 }
